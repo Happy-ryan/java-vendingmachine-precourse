82531052a9aad63a8983d95f05c1c9c5597b9e19
happyryan <tjdgks120813@gmail.com>
2023-12-14 22:53:17 +0900

    docs(readme): add core features for the vending machine.

---
+++ src/main/java/utils/Parser.java
@@ -0,0 +1,24 @@
+package utils;
+
+import java.util.*;
+
+public class Parser {
+    // 김성한,김성한,김성한 -> OutputView에 도움
+    public static String joinWithoutBlank(List<String> stringList) {
+        return String.join(",", stringList);
+    }
+
+    // 김성한, 김성한, 김성한 -> OutputView에 도움
+    public static String joinWithBlank(List<String> stringList) {
+        return String.join(", ", stringList);
+    }
+
+    // List<String> -> List<Integer> 로 변경 -> inputView에 도움
+    public static List<Integer> parseInteger(List<String> stringList) {
+        List<Integer> IntegerList = new ArrayList<>();
+        for (String string : stringList) {
+            IntegerList.add(Integer.parseInt(string));
+        }
+        return IntegerList;
+    }
+}
---
+++ src/main/java/view/OutputView.java
@@ -0,0 +1,22 @@
+package view;
+
+import camp.nextstep.edu.missionutils.Console;
+import utils.Parser;
+
+import java.util.*;
+
+public class OutputView {
+    private static final String a = "";
+    private static final String b = "";
+    private static final String c = "";
+
+    private static final int d = 0;
+
+    public static void print() {
+        System.out.println();
+    }
+
+    public static void print() {
+        System.out.println();
+    }
+}
