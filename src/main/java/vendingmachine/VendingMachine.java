fcbf697d52de51c745dba0be49faf2e8eefee6fa
happyryan <tjdgks120813@gmail.com>
2023-12-15 00:28:34 +0900

    feat(vending): add makeCoinNumber method
    Implemented makeCoinNumber method in VendingMachine to count the occurrences of each coin denomination in the given list of random coins.

--- src/main/java/vendingmachine/VendingMachine.java
+++ src/main/java/vendingmachine/VendingMachine.java
@@ -11,22 +11,38 @@ public class VendingMachine {
         this.vendingMachineMoney = vendingMachineMoney;
     }
 
-    public List<Integer> makeRandCoins(int finalMoney) {
+    public LinkedHashMap<Integer, Integer> makeRandCoins(int finalMoney) {
         List<Integer> coins = new ArrayList<>();
         coins.add(Coin.COIN_10.getAmount());
         coins.add(Coin.COIN_50.getAmount());
         coins.add(Coin.COIN_100.getAmount());
         coins.add(Coin.COIN_500.getAmount());
 
-        List<Integer> randomCoins = new ArrayList<>();
+        List<Integer> randCoins = new ArrayList<>();
         while (finalMoney > 0) {
             int randomCoin = Randoms.pickNumberInList(coins);
             if (finalMoney >= randomCoin) {
-                randomCoins.add(randomCoin);
+                randCoins.add(randomCoin);
                 finalMoney -= randomCoin;
             }
         }
+        return makeCoinNumber(randCoins);
+    }
 
-        return randomCoins;
+    private LinkedHashMap<Integer, Integer> makeCoinNumber(List<Integer> randCoins) {
+        LinkedHashMap<Integer, Integer> coinCounts = new LinkedHashMap<>();
+        coinCounts.put(500, 0);
+        coinCounts.put(100, 0);
+        coinCounts.put(50, 0);
+        coinCounts.put(10, 0);
+
+        for (int coin : randCoins) {
+            coinCounts.put(coin, coinCounts.get(coin) + 1);
+        }
+
+        for (int coin : coinCounts.keySet()) {
+            int count = coinCounts.get(coin);
+        }
+        return coinCounts;
     }
 }
